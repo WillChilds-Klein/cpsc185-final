fc5d9dd503dbb7bd9aa05e2e8d8f8576b705dca9
WillChilds-Klein <willck93@gmail.com>
2015-05-06 21:30:33 -0400

    removed ALL user/authentication nonsense

--- wiki/app.py
+++ wiki/app.py
@@ -10,8 +10,6 @@ from flask import (Flask, render_template, flash, redirect, url_for, request,
 from flask.ext.wtf import Form
 from wtforms import (BooleanField, TextField, TextAreaField, PasswordField)
 from wtforms.validators import (InputRequired, ValidationError)
-from flask.ext.login import (LoginManager, login_required, current_user,
-                             login_user, logout_user)
 from flask.ext.script import Manager
 
 
@@ -32,10 +30,6 @@ except IOError:
            "config.py in your content directory.")
 
 manager = Manager(app)
-
-loginmanager = LoginManager()
-loginmanager.init_app(app)
-loginmanager.login_view = 'user_login'
 
 
 
@@ -240,12 +234,6 @@ class Wiki(object):
             return False
         return Page(path, url, new=True)
 
-    def move(self, url, newurl):
-        os.rename(
-            os.path.join(self.root, url) + '.md',
-            os.path.join(self.root, newurl) + '.md'
-        )
-
     def index(self, attr=None):
         def _walk(directory, path_prefix=()):
             for name in os.listdir(directory):
@@ -309,138 +297,6 @@ class Wiki(object):
         return matched
 
 
-"""
-    User classes & helpers
-    ~~~~~~~~~~~~~~~~~~~~~~
-"""
-
-
-class UserManager(object):
-    """A very simple user Manager, that saves it's data as json."""
-    def __init__(self, path):
-        self.file = os.path.join(path, 'users.json')
-
-    def read(self):
-        if not os.path.exists(self.file):
-            return {}
-        with open(self.file) as f:
-            data = json.loads(f.read())
-        return data
-
-    def write(self, data):
-        with open(self.file, 'w') as f:
-            f.write(json.dumps(data, indent=2))
-
-    def add_user(self, name, password,
-                 active=True, roles=[], authentication_method=None):
-        users = self.read()
-        if users.get(name):
-            return False
-        if authentication_method is None:
-            authentication_method = get_default_authentication_method()
-        new_user = {
-            'active': active,
-            'roles': roles,
-            'authentication_method': authentication_method,
-            'authenticated': False
-        }
-        # Currently we have only two authentication_methods: cleartext and
-        # hash. If we get more authentication_methods, we will need to go to a
-        # strategy object pattern that operates on User.data.
-        if authentication_method == 'hash':
-            new_user['hash'] = make_salted_hash(password)
-        elif authentication_method == 'cleartext':
-            new_user['password'] = password
-        else:
-            raise NotImplementedError(authentication_method)
-        users[name] = new_user
-        self.write(users)
-        userdata = users.get(name)
-        return User(self, name, userdata)
-
-    def get_user(self, name):
-        users = self.read()
-        userdata = users.get(name)
-        if not userdata:
-            return None
-        return User(self, name, userdata)
-
-    def update(self, name, userdata):
-        data = self.read()
-        data[name] = userdata
-        self.write(data)
-
-
-class User(object):
-    def __init__(self, manager, name, data):
-        self.manager = manager
-        self.name = name
-        self.data = data
-
-    def get(self, option):
-        return self.data.get(option)
-
-    def set(self, option, value):
-        self.data[option] = value
-        self.save()
-
-    def save(self):
-        self.manager.update(self.name, self.data)
-
-    def is_authenticated(self):
-        return self.data.get('authenticated')
-
-    def is_active(self):
-        return self.data.get('active')
-
-    def is_anonymous(self):
-        return False
-
-    def get_id(self):
-        return self.name
-
-    def check_password(self, password):
-        """Return True, return False, or raise NotImplementedError if the
-        authentication_method is missing or unknown."""
-        authentication_method = self.data.get('authentication_method', None)
-        if authentication_method is None:
-            authentication_method = get_default_authentication_method()
-        # See comment in UserManager.add_user about authentication_method.
-        if authentication_method == 'hash':
-            result = check_hashed_password(password, self.get('hash'))
-        elif authentication_method == 'cleartext':
-            result = (self.get('password') == password)
-        else:
-            raise NotImplementedError(authentication_method)
-        return result
-
-
-def get_default_authentication_method():
-    return app.config.get('DEFAULT_AUTHENTICATION_METHOD', 'cleartext')
-
-
-def make_salted_hash(password, salt=None):
-    if not salt:
-        salt = os.urandom(64)
-    d = hashlib.sha512()
-    d.update(salt[:32])
-    d.update(password)
-    d.update(salt[32:])
-    return binascii.hexlify(salt) + d.hexdigest()
-
-
-def check_hashed_password(password, salted_hash):
-    salt = binascii.unhexlify(salted_hash[:128])
-    return make_salted_hash(password, salt) == salted_hash
-
-
-def protect(f):
-    @wraps(f)
-    def wrapper(*args, **kwargs):
-        if app.config.get('PRIVATE') and not current_user.is_authenticated():
-            return loginmanager.unauthorized()
-        return f(*args, **kwargs)
-    return wrapper
 
 
 """
@@ -471,31 +327,7 @@ class EditorForm(Form):
     tags = TextField('')
 
 
-class LoginForm(Form):
-    name = TextField('', [InputRequired()])
-    password = PasswordField('', [InputRequired()])
-
-    def validate_name(form, field):
-        user = users.get_user(field.data)
-        if not user:
-            raise ValidationError('This username does not exist.')
-
-    def validate_password(form, field):
-        user = users.get_user(form.name.data)
-        if not user:
-            return
-        if not user.check_password(field.data):
-            raise ValidationError('Username and password do not match.')
-
-
 wiki = Wiki(app.config.get('CONTENT_DIR'))
-
-users = UserManager(app.config.get('CONTENT_DIR'))
-
-
-@loginmanager.user_loader
-def load_user(name):
-    return users.get_user(name)
 
 
 
@@ -506,7 +338,6 @@ def load_user(name):
 
 
 @app.route('/')
-@protect
 def home():
     page = wiki.get('home')
     if page:
@@ -515,21 +346,18 @@ def home():
 
 
 @app.route('/index/')
-@protect
 def index():
     pages = wiki.index()
     return render_template('index.html', pages=pages)
 
 
 @app.route('/<path:url>/')
-@protect
 def display(url):
     page = wiki.get_or_404(url)
     return render_template('page.html', page=page)
 
 
 @app.route('/create/', methods=['GET', 'POST'])
-@protect
 def create():
     form = URLForm()
     if form.validate_on_submit():
@@ -538,7 +366,6 @@ def create():
 
 
 @app.route('/edit/<path:url>/', methods=['GET', 'POST'])
-@protect
 def edit(url):
     page = wiki.get(url)
     form = EditorForm(obj=page)
@@ -553,7 +380,6 @@ def edit(url):
 
 
 @app.route('/preview/', methods=['POST'])
-@protect
 def preview():
     a = request.form
     data = {}
@@ -563,21 +389,18 @@ def preview():
 
 
 @app.route('/tags/')
-@protect
 def tags():
     tags = wiki.get_tags()
     return render_template('tags.html', tags=tags)
 
 
 @app.route('/tag/<string:name>/')
-@protect
 def tag(name):
     tagged = wiki.index_by_tag(name)
     return render_template('tag.html', pages=tagged, tag=name)
 
 
 @app.route('/search/', methods=['GET', 'POST'])
-@protect
 def search():
     form = SearchForm()
     if form.validate_on_submit():
